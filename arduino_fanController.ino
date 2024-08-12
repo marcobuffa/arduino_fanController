b5fd2aab532d2e43a5292a4ee338b98d7365ab6b
Marco Buffa <marcobuffa@gmail.com>
2024-08-12 15:59:25 +0200

    Added DHT11 sensor reading

--- arduino_fanController.ino
+++ arduino_fanController.ino
@@ -1,25 +1,48 @@
+//Using https://github.com/adafruit/DHT-sensor-library
+#include "DHT.h"
+
 #define FAN 2
+#define DHTPIN 4
+#define DHTTYPE DHT11
 #define BAUDRATE 115200
 
 static unsigned int currentTime = 0;
 static unsigned int time;
 
+DHT dht(DHTPIN, DHTTYPE); //sensor
+
 void setup() {
-  pinMode(FAN, OUTPUT);
-  Serial.begin(BAUDRATE);
+  pinMode(FAN, OUTPUT); //initialize fan control pin
+  Serial.begin(BAUDRATE); //initialize serial port
+
+  dht.begin(); //start sensor library
 }
 
 void loop() {
+  float h, t;
+
   time = millis();
   if ((time - currentTime) >= 3000){
     currentTime = time;
+
+    Serial.print(time);
+
+    //read sensor
+    h = dht.readHumidity();
+    t = dht.readTemperature();
+
+
+    Serial.print(" | RH%: ");
+    Serial.print(h);
+    Serial.print(" | T°C: ");
+    Serial.print(t);
+
     if (digitalRead(FAN) == HIGH){
       digitalWrite(FAN, LOW);
-      Serial.println("OFF");
+      Serial.println(" | OFF");
     } else {
       digitalWrite(FAN, HIGH);
-      Serial.println("ON");
+      Serial.println(" | ON");
     }
-    Serial.println(time);
   }
 }
