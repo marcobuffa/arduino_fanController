721ff4751fbc7fb00e95b34f6204f08af8673d80
Marco Buffa <marcobuffa@gmail.com>
2024-08-12 16:15:53 +0200

    First dew point calculation. Sorry we have to go to our grandparents' house for a snack :-)

--- arduino_fanController.ino
+++ arduino_fanController.ino
@@ -9,6 +9,10 @@
 static unsigned int currentTime = 0;
 static unsigned int time;
 
+float dewPoint(float h, float t) {
+    return (t - (14.55 + 0.114 * t) * (1 - (0.01 * h)) - pow(((2.5 + 0.007 * t) * (1 - (0.01 * h))),3) - (15.9 + 0.117 * t) * pow((1 - (0.01 * h)), 14));
+}
+
 DHT dht(DHTPIN, DHTTYPE); //sensor
 
 void setup() {
@@ -19,7 +23,7 @@ void setup() {
 }
 
 void loop() {
-  float h, t;
+  float h, t, dp;
 
   time = millis();
   if ((time - currentTime) >= 3000){
@@ -30,12 +34,14 @@ void loop() {
     //read sensor
     h = dht.readHumidity();
     t = dht.readTemperature();
-
+    dp = dewPoint(h, t);
 
     Serial.print(" | RH%: ");
     Serial.print(h);
     Serial.print(" | T°C: ");
     Serial.print(t);
+    Serial.print(" | DP°C: ");
+    Serial.print(dp);
 
     if (digitalRead(FAN) == HIGH){
       digitalWrite(FAN, LOW);
